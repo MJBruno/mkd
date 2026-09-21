dec8cde4b4f5f3dc1ebd241c19a98ea8713a206a
Mahasolo Jean Bruno <100703949+MJBruno@users.noreply.github.com>
2026-09-21 22:56:40 +0300

    Delete target directory

--- target/debug/deps/html_renderer-22e6873b4ea19589.d
+++
@@ -1,5 +0,0 @@
-C:\Users\BRUNO\Desktop\mkd\mkd\target\debug\deps\html_renderer-22e6873b4ea19589.d: tests\html_renderer.rs
-
-C:\Users\BRUNO\Desktop\mkd\mkd\target\debug\deps\html_renderer-22e6873b4ea19589.exe: tests\html_renderer.rs
-
-tests\html_renderer.rs:
--- target/debug/deps/html_renderer-6ee35378817352f0.d
+++
@@ -1,9 +0,0 @@
-C:\Users\BRUNO\Desktop\mkd\mkd\target\debug\deps\html_renderer-6ee35378817352f0.d: tests\html_renderer.rs Cargo.toml
-
-C:\Users\BRUNO\Desktop\mkd\mkd\target\debug\deps\libhtml_renderer-6ee35378817352f0.rmeta: tests\html_renderer.rs Cargo.toml
-
-tests\html_renderer.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
